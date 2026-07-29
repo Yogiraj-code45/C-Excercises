8c745e3c556bdfcbfe601084385f27ad187736b9
Yogiraj Thombre <yogirajthombre@Yogirajs-MacBook-Air.local>
2026-07-29 22:23:52 +0530

    feat : fix review comments.      - cstring.h -> custom string defination      - cstring.cpp -> implementation to be done

---
+++ cstring.cpp
@@ -0,0 +1,11 @@
+#include "cstring.h"
+
+cstring cstring::append()
+{
+    // TODO 
+}
+
+char cstring::at(int index)
+{
+    // TODO 
+}
---
+++ cstring.h
@@ -0,0 +1,55 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <memory>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <algorithm>
+
+class cstring 
+{
+  protected:
+  char* chr;
+  int currentIndex;
+  std::mutex mutex;
+  int MAX_LENGTH = 1024*1024;
+  
+  public:
+  cstring append(); // length
+  char at(int index); // charAt
+  cstring c_str();
+  int calculate_length(char* len)
+  {
+    int count = 0;
+     while (*len != '0')
+     {
+       len++;
+       count++;
+     }
+     return count;
+  }
+  cstring(char* t) 
+  {
+     chr = new char[calculate_length(t)];
+  }
+  cstring(const cstring& other); // copy constructor 
+  cstring(cstring&& other); // move constructor
+  cstring& operator=(cstring&& other) 
+  {
+    if (this != &other)
+    {
+      chr = other.chr;
+      currentIndex = other.currentIndex;
+      other.chr = nullptr;
+      other.currentIndex = 0;
+    }
+    return *this;
+  }
+  cstring& operator=(const cstring& other) = delete;
+  virtual ~cstring() {
+    delete[] chr;
+  };
+};
+
+#endif
