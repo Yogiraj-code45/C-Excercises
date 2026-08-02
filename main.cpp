99a71887823638651a220b5370f3982fa6cad720
Yogiraj Thombre <yogirajthombre@Yogirajs-MacBook-Air.local>
2026-08-02 21:11:54 +0530

    feat : use of move assignment operator

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@ int main()
     t[1] = 'b';
     t[2] = 'c';
     t[3] = 'd';
+    char* tr = new char[10];
     cstring c(t);
+    cstring nt(tr);
     std::cout << c.c_str() << std::endl;
     std::cout << c.at(2) << std::endl;
     std::cout << c.append(t) << std::endl;
+    std::cout << "Moving the string" << std::endl;
+    nt = std::move(c);
+    std::cout << nt.c_str() << std::endl;
 }
